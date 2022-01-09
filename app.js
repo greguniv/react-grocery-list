8fe0930b705073898f9b2daa014b8090897a3ccf
GREGORY HERNANDEZ UNIVERSE <MAICMP8@HOTMAIL.COM>
2022-01-09 14:07:54 -0600

    have 3 items showing so far

--- app.js
+++ app.js
@@ -1,35 +1,55 @@
 const groceries = [
-	{
-		item: 'Paper Towels',
-		brand: 'Kroger',
-		units: '36 Rolls',
-		quantity: 1,
-		price: 18.99,
-		isPurchased: false,
-	},
-	{
-		item: 'Mac n Cheese',
-		brand: 'Kroger',
-		units: '7.25 oz',
-		quantity: 1,
-		price: 0.55,
-		isPurchased: false,
-	},
-	{
-		item: 'Liquid Dishwashing Soap',
-		brand: 'Dawn',
-		units: '28 fl oz',
-		quantity: 1,
-		price: 3.99,
-		isPurchased: false,
-	},
+    {
+        item: 'Paper Towels',
+        brand: 'Kroger',
+        units: '36 Rolls',
+        quantity: 1,
+        price: 18.99,
+        isPurchased: false,
+    },
+    {
+        item: 'Mac n Cheese',
+        brand: 'Kroger',
+        units: '7.25 oz',
+        quantity: 1,
+        price: 0.55,
+        isPurchased: false,
+    },
+    {
+        item: 'Liquid Dishwashing Soap',
+        brand: 'Dawn',
+        units: '28 fl oz',
+        quantity: 1,
+        price: 3.99,
+        isPurchased: false,
+    },
 ];
+// console.log(groceries)
 
 class App extends React.Component {
-    // state = {}
+    state = {
+        products: groceries,
+        item: '',
+        brand: '',
+        units: '',
+        quantity: 0,
+        price: 0,
+        isPurchased: false,
+    }
 
     render() {
-        return <h1>Hello, we're up and running!</h1>
+        return <h1>Hello, we're up and running!</h1>,
+            <div>
+                <ul>
+                    {
+                        this.state.products.map(item => {
+                            return (
+                                <li>{item.item} {item.quantity} {item.units}</li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
     }
 }
 
